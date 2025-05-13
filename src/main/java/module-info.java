ae7df7cc092252276daa6d0e5b375cf245385f64
Sivapriyan07 <sivapriyan7siva@gmail.com>
2025-05-13 16:40:41 +0530

    removed comment from module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -16,21 +16,7 @@ module com.example.filesharefx {
     opens com.example.filesharefx to javafx.fxml;
     exports com.example.filesharefx;
     requires javafx.graphics;
-    // Add if you use FXML, though not in current code
-    // requires javafx.fxml;
 
-    // Require the Gson module
-
-    // Require the HttpClient module
-
-    // === THIS IS THE CRUCIAL PART ===
     // Open your model package to the Gson module
     opens com.example.filesharefx.model to com.google.gson;
-
-    // You might also need to open other packages if Gson needs to serialize/deserialize
-    // objects from them directly, but for now, 'model' is the one causing issues.
-
-    // Export your main package if it contains the Application class
-    // You might not need to export services if they are only used internally
-    // exports com.example.filesharefx.services;
 }
